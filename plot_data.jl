f4bf8fd6b7445e2bf467b7781f6991178e0d3bfe
qdosgit4 <244482937+qdosgit4@users.noreply.github.com>
2026-04-07 18:16:59 +0000

    Regen graph

--- plot_data.jl
+++ plot_data.jl
@@ -28,7 +28,7 @@ end
 
 function plot_distrib_charts_by_time_outer_loop(res)
         
-    step_set = [0.75, 0.3, 0.3]
+    step_set = [0.6, 0.25, 0.25]
 
     for (i, df) in enumerate(res)
 
@@ -119,6 +119,7 @@ function plot_distrib_charts_by_time(df_full :: DataFrame, file_prefix :: String
         plot_df.counts, 
         group = plot_df.set_label,
         color = plot_df.set_colour,
+        bar_position = :stack,
         xlabel = "Price intervals",
         ylabel = "Frequency",
         title = "Distribution over time (of size $n)",
