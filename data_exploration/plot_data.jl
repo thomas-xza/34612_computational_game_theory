744f984da8627df4a3bc3c66b2685545642bb771
Thomas <122806804+thomas-xza@users.noreply.github.com>
2026-08-01 17:16:47 +0000

    Ported from previous repo

---
+++ data_exploration/plot_data.jl
@@ -0,0 +1,535 @@
+
+
+##  Sample usage:
+##  $ julia plot_data.jl "[1, 3, 10, 25]"
+
+using Pkg
+
+# Pkg.add(["XLSX", "DataFrames", "Plots", "StatsPlots", "StatsBase", "Colors"])
+# Pkg.add(["ShiftedArrays"])
+
+using XLSX
+using Plots, StatsPlots, DataFrames, StatsBase, Colors, ShiftedArrays, JSON
+
+
+##  Demand function (in Jupyter): (u_L - c_L) * (2 - u_L + 0.3 u_F)
+
+##  Demand function (in spec): (u_L - c_L) * (100 - 5 * u_L + 3 * u_F)
+
+
+function main()
+
+    res = DataFrame[]
+
+    xf = XLSX.readxlsx("data.xlsx")
+
+    for sheet in ["Follower_Mk1", "Follower_Mk2", "Follower_Mk3"]
+
+        df = DataFrame(XLSX.readtable("data.xlsx", sheet))
+
+        res = [df, res...]
+
+    end
+
+    n_set = Vector{Int}(JSON.parse(ARGS[1]))
+
+    # plot_time_profitability_1st_deriv(
+    #     res,
+    #     n_set
+    # )
+    
+    # plot_profitability_leader_price_1st_deriv(
+    #     res,
+    #     n_set
+    # )
+    
+    # plot_profitability_leader_price_2nd_deriv(
+    #     res,
+    #     n_set
+    # )
+
+    # plot_profitability_leader_price(
+    #     res,
+    #     n_set
+    # )
+    
+    plot_distrib_charts_by_time_outer_loop(res)
+
+    plot_distrib_charts(res)
+    
+    plot_line_charts(res)
+    
+    plot_diff_data(res)
+    
+end
+
+
+function plot_profitability_leader_price_2nd_deriv(res, n_set ::Vector{Int})
+
+    ranges_y = [(:auto, :auto), (-25, 25), (:auto, :auto)]
+
+    ranges_x = [(:auto, :auto), (-0.3, 0.2), (:auto, :auto)]
+
+    for (i, df) in enumerate(res)
+
+        transform!(df, ["Leader's Price", "Follower's Price", "Cost"] =>
+            ((lp, fp, c) -> (lp .- c) .* (100 .- 5 .* lp .+ 3 .* fp)) => "Profit")
+
+        df[!, "Leader's Price 1st deriv"] = [0; diff(df[!, "Leader's Price"])]
+        
+        df[!, "Leader's Price 2nd deriv"] = [0; diff(df[!, "Leader's Price 1st deriv"])]
+        
+        df[!, "Profit 1st deriv"] = [0; diff(df[!, "Profit"])]
+        
+        df[!, "Profit 2nd deriv"] = [0; diff(df[!, "Profit 1st deriv"])]
+        
+        p = scatter(title = "2nd derivative averages: profitability, leader's price",
+                 xlabel = "Leader's price 2nd deriv. avg.",
+                    ylabel = "Profit 2nd deriv. avg.",
+                 )
+        
+        for n in n_set
+
+            df[!, "Leader's Price 2nd deriv last $(n) avg."] = Float64.(rolling_sum_n(df[!, "Leader's Price 2nd deriv"], n))
+            
+            df[!, "Profit 2nd deriv last $(n) avg."] = Float64.(rolling_sum_n(df[!, "Profit 2nd deriv"], n))
+
+            scatter!(
+                p,
+                df[!, "Leader's Price 2nd deriv last $(n) avg."],
+                df[!, "Profit 2nd deriv last $(n) avg."],
+                label="MK$(i), batch $(n)",
+                markeralpha = 0.3,
+            )
+
+        end
+
+        # println(df)
+        
+        savefig(p, "profitability_leader_price_MK$(i)_2nd_deriv_avgs_spec_demand_model.svg")
+
+    end
+
+end
+
+
+function plot_profitability_leader_price_1st_deriv(res, n_set ::Vector{Int})
+
+    ranges_y = [(:auto, :auto), (-25, 25), (:auto, :auto)]
+
+    ranges_x = [(:auto, :auto), (-0.15, 0.15), (:auto, :auto)]
+
+    for (i, df) in enumerate(res)
+
+        transform!(df, ["Leader's Price", "Follower's Price", "Cost"] =>
+            ((lp, fp, c) -> (lp .- c) .* (100 .- 5 .* lp .+ 3 .* fp)) => "Profit")
+
+        df[!, "Leader's Price 1st deriv"] = [0; diff(df[!, "Leader's Price"])]
+
+        df[!, "Profit 1st deriv"] = [0; diff(df[!, "Profit"])]
+
+        # println(df)
+        
+        p = scatter(title = "1st derivative averages: profitability, leader's price",
+                 xlabel = "Leader's price 1st deriv avg.",
+                 ylabel = "Profit 1st deriv avg.",
+                 )
+        
+        for n in n_set
+
+            df[!, "Leader's Price 1st deriv last $(n) avg."] = Float64.(rolling_sum_n(df[!, "Leader's Price 1st deriv"], n))
+ 
+            df[!, "Profit 1st deriv last $(n) avg."] = Float64.(rolling_sum_n(df[!, "Profit 1st deriv"], n))
+ 
+            scatter!(
+                p,
+                df[!, "Leader's Price 1st deriv last $(n) avg."],
+                df[!, "Profit 1st deriv last $(n) avg."],
+                label="MK$(i), batch $(n)",
+                markeralpha = 0.3,
+            )
+
+        end
+
+        savefig(p, "profitability_leader_price_MK$(i)_1st_deriv_avg_spec_demand_model.svg")
+
+    end
+
+end
+
+
+function plot_time_profitability_1st_deriv(res, n_set ::Vector{Int})
+
+    ranges = [(:auto, :auto), (:auto, :auto), (:auto, :auto)]
+
+    for (i, df) in enumerate(res)
+
+        transform!(df, ["Leader's Price", "Follower's Price", "Cost"] =>
+            ((lp, fp, c) -> (lp .- c) .* (100 .- 5 .* lp .+ 3 .* fp)) => "Profit")
+
+        df[!, "Profit 1st deriv"] = [0; diff(df[!, "Profit"])]
+
+        # println(df)
+
+        p = scatter(
+            title = "1st derivative averages over time for: profitability",
+            xlabel = "Date",
+            ylabel = "Profit 1st deriv avg.",
+        )
+        
+        for n in n_set
+
+            df[!, "Profit 1st deriv last $(n) avg."] = Float64.(rolling_sum_n(df[!, "Profit 1st deriv"], n))
+ 
+            scatter!(
+                p,
+                df[!, "Date"],
+                df[!, "Profit 1st deriv last $(n) avg."],                 
+                # ylims = ranges[i],
+                label="MK$(i), batch $(n)",
+                markeralpha = 0.6,
+            )
+
+        end
+
+        savefig(p, "profitability_1st_deriv_time_MK$(i)_spec_demand_model.svg")
+
+    end
+
+end
+
+
+
+function plot_profitability_leader_price(res, n_set ::Vector{Int})
+
+    ranges = [(:auto, :auto), (:auto, 90), (:auto, :auto)]
+
+    n = 1
+
+    for (i, df) in enumerate(res)
+
+        transform!(df, ["Leader's Price", "Follower's Price", "Cost"] =>
+            ((lp, fp, c) -> (lp .- c) .* (100 .- 5 .* lp .+ 3 .* fp)) => "Profit")
+
+        p = plot(df[!, "Leader's Price"],
+                 df[!, "Profit"],
+                 title = "Correaltion between leader's price and profit",
+                 xlabel = "Leader's price",
+                 ylabel = "Profit",
+                 ylims = ranges[i],
+                 label="MK$(i), batch $(n)",
+                 seriestype = :scatter)
+        
+        savefig(p, "profitability_leader_price_MK$(i)_spec_demand.svg")
+
+    end
+
+end
+
+
+function rolling_sum_n(v, n :: Int)
+    
+    res = fill(Float64(0), length(v))
+
+    for i in n:length(v)
+
+        for j in 0:(n - 1)
+        
+            res[i] += v[i - j]
+
+            # println(res[i])
+
+        end
+
+        res[i] = res[i] / n
+        
+    end
+    
+    return res
+    
+end
+
+
+function plot_profitability_over_time(res)
+
+    ranges = [(:auto, :auto), (:auto, 90), (:auto, :auto)]
+
+    for (i, df) in enumerate(res)
+
+        ##  Demand function (in Jupyter): (u_L - c_L) * (2 - u_L + 0.3 u_F)
+
+        ##  Demand function (in spec): (u_L - c_L) * (100 - 5 * u_L + 3 * u_F)
+
+        transform!(df, ["Leader's Price", "Follower's Price", "Cost"] =>
+            ((lp, fp, c) -> (lp .- c) .* (100 .- 5 .* lp .+ 3 .* fp)) => "Profit")
+
+        println(df)
+
+        p = plot(df[!, "Date"],
+                 df[!, "Profit"],
+                 title = "Correlation between profitability and date (MK$i)",
+                 xlabel = "Date",
+                 ylabel = "Profit",
+                 ylims = ranges[i],
+                 seriestype = :line)
+
+        savefig(p, "profitability_over_time_$(i)_pdf_demand_model.svg")
+
+    end
+
+end
+
+
+function plot_profitability_leader_price(res)
+
+    ranges = [(:auto, :auto), (:auto, 90), (:auto, :auto)]
+
+    for (i, df) in enumerate(res)
+
+        ##  Demand function (in Jupyter): (u_L - c_L) * (2 - u_L + 0.3 u_F)
+
+        ##  Demand function (in spec): (u_L - c_L) * (100 - 5 * u_L + 3 * u_F)
+
+        transform!(df, ["Leader's Price", "Follower's Price", "Cost"] =>
+            ((lp, fp, c) -> (lp .- c) .* (100 .- 5 .* lp .+ 3 .* fp)) => "Profit")
+
+        println(df)
+
+        p = plot(df[!, "Leader's Price"],
+                 df[!, "Profit"],
+                 title = "Correlation between profitability and leader's price (MK$i)",
+                 xlabel = "Leader's price",
+                 ylabel = "Profit",
+                 ylims = ranges[i],
+                 seriestype = :scatter)
+
+        savefig(p, "profitability_leader_price_$(i)_pdf_demand_model.svg")
+
+    end
+
+end
+
+
+function plot_distrib_charts_by_time_outer_loop(res)
+        
+    step_set = [0.06, 0.25, 0.25]
+
+    for (i, df) in enumerate(res)
+
+        println(i)
+
+        plot_distrib_charts_by_time(df, string(i), step_set[i])
+
+    end
+
+end
+
+
+function plot_distrib_charts_by_time(df_full :: DataFrame, file_prefix :: String, step :: Float64)
+    
+    absolute_min = min(minimum(df_full[!, "Leader's Price"]), minimum(df_full[!, "Follower's Price"]))
+
+    floor_min = floor(absolute_min / step) * step
+
+    absolute_max = max(maximum(df_full[!, "Leader's Price"]), maximum(df_full[!, "Follower's Price"]))
+
+    println(absolute_max)
+
+    if absolute_max > 25
+
+        absolute_max = 3.25
+
+    end
+
+    ceil_max = ceil(absolute_max / step) * (step)
+
+    edges = (floor_min:step:ceil_max)
+
+    ##  The above is generic to the dataframe as a whole, before splitting.
+
+    n = 25
+
+    plot_df = DataFrame()
+
+    reds = Dict("Leader" => 1, "Follower" => 0)
+
+    for target in ["Leader", "Follower"]
+    
+        r, g, b = reds[target], 0, 0
+
+        col_to_plot = "$target's Price"
+        
+        for (i, start_idx) in enumerate(1:n:nrow(df_full))
+
+            g = 0.2 * i
+
+            b = 0.1 * i
+
+            end_idx = min(start_idx + n - 1, nrow(df_full))
+            
+            row_set = df_full[start_idx:end_idx, col_to_plot]
+            
+            h = fit(Histogram, row_set, edges)
+            
+            centers = [ (edges[j] + edges[j+1]) / 2 for j in 1:length(h.weights) ]
+            
+            temp_df = DataFrame(
+                bin_center = centers,
+                counts = h.weights,
+                set_label = "Date $start_idx-$end_idx, $target",
+                set_colour = RGB(r, g, b)
+            )
+            append!(plot_df, temp_df)
+            
+        end
+
+    end
+
+    plot_df.set_colour = parse.(Colorant, plot_df.set_colour)
+
+    unique_data = unique(plot_df[!, [:set_label, :set_colour]])
+    group_labels = unique_data.set_label
+    group_colors = unique_data.set_colour
+
+    println(group_labels)
+    
+    println(group_colors)
+    
+    println(plot_df)
+
+    # 3. Plot side-by-side using groupedbar
+    gb = groupedbar(
+        plot_df.bin_center, 
+        plot_df.counts, 
+        group = plot_df.set_label,
+        color = plot_df.set_colour,
+        xlabel = "Price intervals",
+        ylabel = "Frequency",
+        title = "Distribution over time (of size $n)",
+        legend = :outertopright
+    )
+    
+    savefig(gb, "price_distribs_split_MK$(file_prefix).svg")
+
+end
+
+
+function split_df_by_time(df, n)
+
+    split_dfs = [DataFrame(row_group) for row_group in Iterators.partition(eachrow(df), n)]
+
+    return split_dfs
+
+end
+
+
+function plot_distrib_charts(res)
+
+    for (i, df) in enumerate(res)
+
+        step = 0.025
+
+        absolute_min = min(minimum(df[!, "Leader's Price"]), minimum(df[!, "Follower's Price"]))
+
+        floor_min = floor(absolute_min / step) * step
+
+        absolute_max = max(maximum(df[!, "Leader's Price"]), maximum(df[!, "Follower's Price"]))
+
+        if i == 2
+
+            absolute_max = 3.25
+
+        end
+
+        floor_max = floor(absolute_max / step) * step
+
+        edges = (floor_min:step:floor_max)
+
+        h1 = fit(Histogram, df[!, "Leader's Price"], edges).weights
+        h2 = fit(Histogram, df[!, "Follower's Price"], edges).weights
+
+        labels = ["$i" for i in edges[1:end-1]]
+
+        gb = groupedbar(labels,
+                        [h1 h2], 
+                        label = ["Leader" "Follower"],
+                        title = "Price distributions, MK$i",
+                        xlabel = "Price Interval",
+                        ylabel = "Frequency",
+                        xtickfont = 4, 
+                        xrotation = 90
+                        )
+                        # bar_width = 0.7)
+ 
+        savefig(gb, "price_distribs_MK$(i).svg")
+
+    end
+
+end
+
+
+function plot_line_charts(res)
+
+    max_y_lims = [:auto, (-2, 3.5), :auto]
+
+    for (i, df) in enumerate(res)
+
+        transform!(df, ["Leader's Price", "Follower's Price"] => ((lp, fp) -> lp .- fp) => "Diff")
+        
+        p = plot(df[!, "Date"], df[!, "Leader's Price"], 
+             label = "Leader", 
+             xlabel = "Date", 
+             ylabel = "Price", 
+             title = "Price changes over time",
+             linewidth = 2)
+
+        # 2. Add the second line to the same plot
+        plot!(p,
+              df[!, "Date"],
+              df[!, "Follower's Price"], 
+              label = "Follower", 
+              linewidth = 2,
+              ylims = max_y_lims[i]
+              )
+ 
+        plot!(p,
+              df[!, "Date"],
+              df[!, "Diff"], 
+              label = "Difference", 
+              linewidth = 2,
+              ylims = max_y_lims[i]
+              )
+ 
+        savefig(p, "lines_prices_time_MK$(i).svg")
+
+    end    
+
+end
+
+
+function plot_diff_data(res)
+
+    ranges = [
+        (0.2:0.01:0.7),
+        (-1.5:0.03:0.5),
+        (-2:0.02:-1)
+    ]
+
+    for (i, df) in enumerate(res)
+ 
+        transform!(df, ["Leader's Price", "Follower's Price"] => ((lp, fp) -> lp .- fp) => "Diff")
+
+        h = histogram(df[!, "Diff"],
+                      bins=ranges[i],
+                      title="Difference between leader and follower price",
+                      xlabel="Price interval",
+                      ylabel="Frequency")
+
+        savefig(h, "hist_leader_follower_price_diff_MK$(i).svg")
+
+    end
+
+end
+
+
+main()
---
+++ data_exploration/plot_diff_data.jl
@@ -0,0 +1,46 @@
+
+using Pkg
+
+##  Pkg.add(["XLSX", "DataFrames", "Plots"])
+
+using XLSX
+using Plots, DataFrames
+
+
+function main()
+
+    res = DataFrame[]
+
+    xf = XLSX.readxlsx("data.xlsx")
+
+    for sheet in ["Follower_Mk1", "Follower_Mk2", "Follower_Mk3"]
+
+        df = DataFrame(XLSX.readtable("data.xlsx", sheet))
+
+        res = [df, res...]
+
+    end
+
+    ranges = [
+        (0.2:0.01:0.7),
+        (-1.5:0.03:0.5),
+        (-2:0.02:-1)
+    ]
+
+    for (i, df) in enumerate(res)
+ 
+        transform!(df, ["Leader's Price", "Follower's Price"] => ((lp, fp) -> lp .- fp) => "Diff")
+
+        h = histogram(df[!, "Diff"],
+                      bins=ranges[i],
+                      title="Difference between leader and follower price",
+                      xlabel="Price interval",
+                      ylabel="Frequency")
+
+        savefig(h, "hist_$i.pdf")
+
+    end
+
+end
+
+main()
